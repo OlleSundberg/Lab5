12245e73d9593d531560e423ff35e397844b29c3
Olle Sundberg <olle.sundbergg@gmail.com>
2017-10-19 10:39:16 +0200

    Update User.cs

--- Lab5/Lab5/User.cs
+++ Lab5/Lab5/User.cs
@@ -8,5 +8,14 @@ namespace Lab5
 {
     class User
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        bool isAdmin = false;
+
+        public User(string Name, string Email)
+        {
+            this.Name = Name;
+            this.Email = Email;
+        }
     }
 }
